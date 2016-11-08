92543b0c88f47dd4df2c89ae10f876f600f28264
maventurer <molivertauler@gmail.com>
2016-11-08 12:23:51 +0100

    Player movement

---
+++ Game_Jam_2016/Assets/Code/InputManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class InputManager {
+
+    //private static Touch touch = Input.GetTouch(0);
+
+    public static bool MoveRight()
+    {
+        return Input.touchCount > 0 && Input.GetTouch(0).position.x > Screen.width / 2;
+    }
+
+    public static bool MoveLeft()
+    {
+        return Input.touchCount > 0 && Input.GetTouch(0).position.x < Screen.width / 2;
+    }
+}
--- Game_Jam_2016/Assets/Code/PlayerBehaviour.cs
+++ Game_Jam_2016/Assets/Code/PlayerBehaviour.cs
@@ -3,13 +3,39 @@ using System.Collections;
 
 public class PlayerBehaviour : MonoBehaviour {
 
+    private Vector3 movement;
+
+    public float lerpTime;
+    public float lateralAcceleration;
+
 	// Use this for initialization
+    void Awake ()
+    {
+
+    }
+
 	void Start () {
-	
+        movement = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Movement();
 	}
+
+    #region functions
+
+    void Movement()
+    {
+        if(InputManager.MoveLeft())
+            movement.x -= lateralAcceleration;
+        else if(InputManager.MoveRight())
+            movement.x += lateralAcceleration;
+        else
+            movement = Vector3.zero;
+
+        this.transform.position = Vector3.Lerp(this.transform.position, this.transform.position + movement, lerpTime);
+    }
+
+    #endregion
 }
