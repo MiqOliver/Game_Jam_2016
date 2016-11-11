fbef2ed3ab1770088f2daab81be9faf5519f73b3
DOSSM <marcssegarra@gmail.com>
2016-11-11 12:46:18 +0100

    Fix GenerateObstacles

--- Game_Jam_2016/Assets/Code/GenerateMapObstacles.cs
+++ Game_Jam_2016/Assets/Code/GenerateMapObstacles.cs
@@ -15,6 +15,9 @@ public class GenerateMapObstacles : MonoBehaviour {
     public float minTime;
     public float maxTime;
 
+    private int count;
+    private float extraTime;
+
     void Awake()
     {
         distanceXBetween = Random.Range(distanceXBetweenMin, distanceXBetweenMax);
@@ -22,15 +25,54 @@ public class GenerateMapObstacles : MonoBehaviour {
 
     void Start()
     {
+        extraTime = 0;
         GenerateObstacles();
+    } 
+
+    IEnumerator ShootAnother(float s)
+    {
+        yield return new WaitForSeconds(s);
+        GenerateObstacle();
     }
 
     void GenerateObstacles()
     {
+        int obj = Random.Range(0, objs.Length);
+        if (count >= 3)
+        {
+            Debug.Log("1");
+            if (Random.Range(0, 2) == 1)
+            {
+                Instantiate(objs[1], transform.position + new Vector3(1.5f, 0, 0), Quaternion.identity);
+                count = 0;
+            }
+            else
+            {
+                Instantiate(objs[1], transform.position + new Vector3(-1.5f, 0, 0), Quaternion.identity);
+                count = 0;
+            }
+        }
+        else
+        {
+            Instantiate(objs[obj], transform.position + new Vector3(distanceXBetween, 0, 0), Quaternion.identity);
+            distanceXBetween = Random.Range(distanceXBetweenMin, distanceXBetweenMax);
+            count++;
 
-        Instantiate(objs[Random.Range(0, objs.Length)], transform.position + new Vector3(distanceXBetween,0,0), Quaternion.identity);
 
-        distanceXBetween = Random.Range(distanceXBetweenMin, distanceXBetweenMax);
-        Invoke("GenerateObstacles", Random.Range(minTime, maxTime));
+        }
+        if (obj != 1)
+        {
+            StartCoroutine(ShootAnother(1f));
+            extraTime = 1;
+            count++;
+        }
+        Invoke("GenerateObstacles", Random.Range(minTime, maxTime) + extraTime);
+        extraTime = 0;
+    }
+
+    void GenerateObstacle()
+    {
+        Instantiate(objs[Random.Range(0, objs.Length)], transform.position + new Vector3(distanceXBetween, 0, 0), Quaternion.identity);
+        count++;
     }
 }
