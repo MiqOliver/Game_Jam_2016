e9fc69fc11c950fe47460f016914e324974b5801
Manpos <miquelpostigo@gmail.com>
2016-11-11 13:15:21 +0100

    Menus

--- Game_Jam_2016/Assets/Code/GameState.cs
+++ Game_Jam_2016/Assets/Code/GameState.cs
@@ -26,17 +26,32 @@ public class GameState : MonoBehaviour
         else if(speed > maxSpeed)
             speed = maxSpeed;
 
+        Restart();
+    }
+
+    public void Restart()
+    {
         //Restarting Scene
         if (GameObject.FindWithTag("Player") == null)
         {
             SceneManager.LoadScene(0, LoadSceneMode.Single);
         }
     }
+    public void RestartButton()
+    {
+
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+        Time.timeScale = 1;
+    }
 
     public void AddScore()
     {
         score++;
         UpdateScore();
+    }
+    public void Quit()
+    {
+        Application.Quit();
     }
 
     void UpdateScore()
